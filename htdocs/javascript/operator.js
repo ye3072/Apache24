68884de0c099e2f413db69191b23cb35d28580f6
YeongeunLee <48782880+ye3072@users.noreply.github.com>
2020-09-15 15:06:44 +0900

    Update operator.js

--- htdocs/javascript/operator.js
+++ htdocs/javascript/operator.js
@@ -10,3 +10,31 @@ console.log(`string literals: 1 + 2 = ${1 + 2}`);
  console.log(1 * 1); // multiply
  console.log(5 % 2); // remainder
  console.log(2 ** 3); // exponentiation
+
+ // 3. Increment and decrement operators
+ let counter = 2;
+ const preIncrement = ++counter;
+ console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+
+ const postIncrement = counter++;
+ console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+
+
+ // 4. Assignment operators
+ let x = 3;
+ let y = 6;
+
+ // 5. Logical operators: || (or), && (and), ! (not)
+ const value1 = false;
+ const value2 = 4 < 2;
+
+ // || (or), finds the first truthy value
+ console.log(`or: ${value1 || value2 || check()}`);
+
+ function check() {
+	 for (let i = 0; i < 10; i++) {
+		 //wasting time
+		 console.log('😱');
+	 }
+	 return true;
+ }
